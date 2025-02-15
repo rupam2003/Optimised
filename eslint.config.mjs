ff364eac24043f02c21410b0c7aecf9b944fb0f8
rupam2003 <rupambhattacharya0@gmail.com>
2025-02-16 00:58:37 +0530

    eslint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-var': 'off',
+    },
+  }),
+]
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 
 export default eslintConfig;
