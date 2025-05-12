5e94b4fb71f669d7c42206b6bc3f4620817b44a9
rupam2003 <rupambhattacharya0@gmail.com>
2025-05-12 19:43:01 +0530

    smaller logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,32 +12,32 @@ import { Button } from './ui/button'
 
 const Header = () => {
 
-    const session = useQuery(api.users.viewer)
-    return (
+  const session = useQuery(api.users.viewer)
+  return (
     <header className='font-bold border-b-[1px] backdrop-blur-2xl bg-opacity-20 z-50 sticky top-0   w-full  bg-black  '>
       <div className='mx-auto flex justify-between items-center max-w-[650px]'>
-        
-        <div className='flex justify-center items-center gap-1 ml-1 m-1.5'>
-          <figure className='w-8 h-8 sm:w-9 sm:h-9  relative '>
-            <Image className='rounded-full' src={"\code.png"} fill alt={"profile"}/>
+
+        <div className='flex justify-center items-center gap-1 ml-1 m-2.5'>
+          <figure className='w-7 h-7 relative '>
+            <Image className='rounded-full' src={"\code.png"} fill alt={"profile"} />
           </figure>
-          <h1 className='font-semibold text-lg sm:text-xl '>Optimised</h1>
+          <h1 className='font-semibold text-lg '>Optimised</h1>
         </div>
-        
+
         {
           <div className='flex items-center gap-2 mr-1 '>
             {
-                session?.user?.image && 
-                <div className='w-8 h-8 sm:w-9 sm:h-9  relative '>
-                    <Image className='rounded-full' src={session?.user?.image} fill objectFit='contain' alt={"profile"}/>
-                </div>
+              session?.user?.image &&
+              <div className='w-8 h-8 sm:w-9 sm:h-9  relative '>
+                <Image className='rounded-full' src={session?.user?.image} fill objectFit='contain' alt={"profile"} />
+              </div>
             }
-            {session?.status == "authenticated" && <SignOut/>}
-            {session?.status == "unauthenticated" && <SignInWithGoogle/>}
-        </div>
- 
+            {session?.status == "authenticated" && <SignOut />}
+            {session?.status == "unauthenticated" && <SignInWithGoogle />}
+          </div>
+
         }
-      </div>  
+      </div>
     </header>
   )
 }
@@ -46,49 +46,48 @@ export default Header
 
 
 function SignOut() {
-    const session = useQuery(api.users.viewer)
-    const router = useRouter()
-    const { signOut } = useAuthActions();
-    useEffect(() => {
-      if(session?.status == "unauthenticated")
-        router.push("/")
-      
-    }, [session,router])
-    return (
-      <Button
-        className="flex-1"
-        variant="outline"
-        type="button"
-        onClick={() => {
-          signOut()    
-        }}
-      >
-        Sign Out
-      </Button>
-    );
-  }
+  const session = useQuery(api.users.viewer)
+  const router = useRouter()
+  const { signOut } = useAuthActions();
+  useEffect(() => {
+    if (session?.status == "unauthenticated")
+      router.push("/")
+
+  }, [session, router])
+  return (
+    <Button
+      className="flex-1"
+      variant="outline"
+      type="button"
+      onClick={() => {
+        signOut()
+      }}
+    >
+      Sign Out
+    </Button>
+  );
+}
 
 function SignInWithGoogle() {
-    const { signIn } = useAuthActions();
-    const session = useQuery(api.users.viewer)
-    const router = useRouter()
-    useEffect(() => {
-      if(session?.status == "unauthenticated")
-       {
-         router.push("/")
-         router.refresh()
-      } 
-        
-      
-    }, [session])
-    return (
-      <Button
-        className="flex-1"
-        variant="outline"
-        type="button"
-        onClick={() => void signIn("google", { redirectTo: "/" })}
-      >
-        Sign In
-      </Button>
-    );
-  }
+  const { signIn } = useAuthActions();
+  const session = useQuery(api.users.viewer)
+  const router = useRouter()
+  useEffect(() => {
+    if (session?.status == "unauthenticated") {
+      router.push("/")
+      router.refresh()
+    }
+
+
+  }, [session])
+  return (
+    <Button
+      className="flex-1"
+      variant="outline"
+      type="button"
+      onClick={() => void signIn("google", { redirectTo: "/" })}
+    >
+      Sign In
+    </Button>
+  );
+}
